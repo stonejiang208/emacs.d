dda64b5bd8b69edb62ac5482ab960353481cb738
emuio <emuio@hotmail.com>
2015-08-06 15:37:58 +0800

    init-clock

---
+++ lisp/init-clock.el
@@ -0,0 +1,8 @@
+;;----------------------------------------------------------------------------
+;;tea-time
+;;----------------------------------------------------------------------------
+(require 'tea-time)
+(setq tea-time-sound "~/Music/ring.m4r")
+(setq tea-time-sound-command "afplay %s")
+
+(provide 'init-clock)
