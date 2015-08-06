ba94fda27c40d70cd92d25f4f66fc7d0fe9e9be0
emuio <emuio@hotmail.com>
2015-08-06 15:45:34 +0800

    show time on minibuffer

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -83,4 +83,20 @@
 ;;----------------------------------------------------------------------------
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 
+;;----------------------------------------------------------------------------
+;; 显示时间
+;;----------------------------------------------------------------------------
+;;启用时间显示设置，在minibuffer上面的那个杠上
+(display-time-mode 1)
+;;时间使用24小时制
+(setq display-time-24hr-format t)
+;;时间显示包括日期和具体时间
+(setq display-time-day-and-date t)
+;;时间栏旁边启用邮件设置
+(setq display-time-use-mail-icon nil)
+;;时间的变化频率
+(setq display-time-interval 10)
+;;显示时间的格式
+;;(setq display-time-format nil)
+
 (provide 'init-gui-frames)
