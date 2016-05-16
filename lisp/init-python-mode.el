cd7fd3ca251f0792db0183ddab83a6b6f2d8fb7e
emuio <emuio@hotmail.com>
2016-05-16 13:50:50 +0800

    add elpy

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,9 +1,13 @@
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
-		("SConscript\\'" . python-mode))
+                ("SConscript\\'" . python-mode))
               auto-mode-alist))
 
 (require-package 'pip-requirements)
 
+(add-to-list 'package-archives
+             '("elpy" . "https://jorgenschaefer.github.io/packages/"))
+(require-package 'elpy)
+(elpy-enable)
 
 (provide 'init-python-mode)
