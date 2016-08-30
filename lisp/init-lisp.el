c01969ccdeb389626493ef8af35bad800987893f
emuio <emuio@hotmail.com>
2016-08-30 13:44:30 +0800

    add slime-helper for quicklisp

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -306,4 +306,9 @@
 
 (maybe-require-package 'cask-mode)
 
+;; slime-helper
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
+(setq inferior-lisp-program "sbcl")
+
 (provide 'init-lisp)
