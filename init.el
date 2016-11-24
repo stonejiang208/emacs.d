7fc748d81cfb24801c06cb9e89a8de00641ac5ed
emuio <emuio@hotmail.com>
2016-11-24 13:54:58 +0800

    add protobuf-mode

--- init.el
+++ init.el
@@ -142,6 +142,7 @@
 (when *is-a-mac*
   (require-package 'osx-location))
 (require-package 'regex-tool)
+(require-package 'protobuf-mode)
 
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
