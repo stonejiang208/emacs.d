895db837466b7776e0e4503dccb4848d8a7aeca5
jiangtao <jiangtao@tao-studio.net>
2017-05-29 21:42:59 +0800

    mpc and template

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,9 +1,24 @@
-;;; Package --- userconfig
+;;; init-local.el --- userconfig
 ;;; Commentary:
 
 ;;; Code:
-
-
+(setq c-default-style "ellemtel")
+(require 'mpc-mode)
+(require 'template)
+(require 'time-stamp)
+(setq time-stamp-active t)
+(setq time-stamp-warn-inactive t)
+(add-hook 'write-file-hooks 'time-stamp)
+(setq time-stamp-format "%04y-%02m-%02d %02H:%02M:%02S by %u")
+;;
+(require 'template)
+(template-initialize)
+(setq template-default-directories
+      '("~/.emacs.d/templates/"))
+(setq select-enable-clipboard t)
+(setq tab-width 2)
+(setq default-tab-width 2)
+(setq c-basic-offset 2)
 
 ;;----------------------------------------------------------------------------
 (provide 'init-local)
